9c2565619a47aac9c8ec6ef0362d24ad1e6ed462
FrancisHenriquez <francishenriquez07@gmail.com>
2022-12-20 15:03:52 -0400

    first validation

--- controllers/authControlles.js
+++ controllers/authControlles.js
@@ -4,17 +4,33 @@ const { response } = require('express');
 
 const registerUser = (req, res = response) => {
 
+    const { name, email, password } = req.body;
+
+    if (name.length < 5){
+        return res.status(400).json({
+            ok:false,
+            msg:'el nombre debe tener minimo 5 letras'
+        })
+    }
+
     res.json ({
         ok: true,
-        msg: 'register'
+        msg: 'register',
+        name,
+        email,
+        password,
     })
 }
 
 const login = (req, res = response) => {
+
+    const { email, password } = req.body
     console.log(' se requiere /')
     res.json ({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,  
+        password
     })
 }
 
--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 //* Public directory
 app.use(express.static('public'));
 
+//* lecture and body parse 
+app.use( express.json() );
+
 //Routes
 app.use('/api/auth', require('./routes/auth'));
 
